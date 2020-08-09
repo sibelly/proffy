62549611a032a106cbf0800ca07eecfa5ce551db
sibelly <sibellychan@hotmail.com>
2020-08-09 01:09:47 -0400

    Adds the increment connections functionality and whatsapp button

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,40 +3,58 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+export interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img
-          src="https://media-exp1.licdn.com/dms/image/C4D03AQHUNZwqgPIr7Q/profile-displayphoto-shrink_200_200/0?e=1602115200&v=beta&t=LyNhG9CAE0Dk4n2yodU0CITvFMwfTMpoyJFKWbavHLQ"
-          alt="Sibs"
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Sibelly Sanches</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-      <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br />
-        <br />
-        Apaixonada por explodir coisas em laboratório e por mudar a vida das
-        pessoas através de experiência. Mais de 200 mil pessoas já passaram
-        pelas minhas explosões.
-      </p>
+      <p>{teacher.bio}</p>
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>
+            R$
+            {teacher.cost}
+          </strong>
         </p>
-        <button type="button">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
-}
+};
 
 export default TeacherItem;
